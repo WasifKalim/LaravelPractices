b5e0cbfa48d130c292389850e64428104adb3efb
Wasifkalimkalim <wasifnit2k19@gmail.com>
2023-09-28 11:33:09 +0530

    uplaod file2

--- Upload-file/routes/web.php
+++ Upload-file/routes/web.php
@@ -1,8 +1,9 @@
 <?php
-
+use ErlandMuchasaj\UploadFile\FileUploader; // <= import the package
 use App\Http\Controllers\FileController;
 use App\Http\Controllers\UploadManager;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,39 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/w', [FileController::class, 'index']);
 Route::post('/submitform',[FileController::class,'submitform'])->name('submitform');
+
+
+
+
+
+// visualize the form
+Route::get('/files', function (Request $request) {
+    return view('files');
+})->name('files');
+
+// handle the post request
+Route::post('/files', function (Request $request) {
+
+    $max_size = (int) ini_get('upload_max_filesize') * 1000;
+
+    $extensions = implode(',', F::images());
+
+    $request->validate([
+        'file' => [
+            'required',
+            'file',
+            'image',
+            'mimes:' . $extensions,
+            'max:'.$max_size,
+        ]
+    ]);
+
+    $file = $request->file('file');
+
+    $response = FileUploader::store($file);
+
+    return redirect()
+            ->back()
+            ->with('success','File has been uploaded.')
+            ->with('file', $response);
+})->name('files.store');
